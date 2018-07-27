59d1f01d333efcf2788625a394e1143c8de5ab61
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-26 21:40:56 -0500

    Master code

--- CG 3-2/Program.cs
+++ CG 3-2/Program.cs
@@ -6,7 +6,19 @@ namespace CG_3_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello.");
+            Console.WriteLine("Do you want to play a game?");
+            Console.WriteLine("I'm thinking of a number 1 thru 10.");
+            Console.WriteLine("Guess correctly and you win.");
+            Console.WriteLine("Guess wrong and you lose.");
+            Console.WriteLine("Let's begin.");
+
+            string guess = Console.ReadLine();
+            string message = (guess == "8") ? "You win!" : "You Lose!";
+
+            Console.ReadLine();
+
+           
         }
     }
 }
