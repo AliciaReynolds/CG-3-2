12aa8d87597e2feca3b25ba051da1b748ccfb0c5
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-26 22:12:08 -0500

    Add comments

--- CG 3-2/Program.cs
+++ CG 3-2/Program.cs
@@ -6,15 +6,30 @@ namespace CG_3_2
     {
         static void Main(string[] args)
         {
+            /*Opening up the program with lines to explain how the 
+             * 'game' is played. */
+
+
             Console.WriteLine("Hello.");
             Console.WriteLine("Do you want to play a game?");
             Console.WriteLine("I'm thinking of a number 1 thru 10.");
             Console.WriteLine("Guess correctly and you win.");
             Console.WriteLine("Guess wrong and you lose.");
-            Console.WriteLine("Let's begin.");
+            Console.WriteLine("Please enter your guess.");
+
+            /*This allows the user to input what they are guessing.*/
+
 
             string guess = Console.ReadLine();
             
+            /*I originally was having issues with this bit of code beacause after
+             * I had the information inputed nothing would write out on the 
+             * screen.  In my class notes, I didn't write down that you had
+             * to include the console.writeline command. I thought that maybe
+             * that was what I was missing but I went back and watched
+             * Bobo to verify that thought. */
+
+
             string message = (guess == "8") ? "You win!" : "You Lose!";
 
             Console.WriteLine(message);
